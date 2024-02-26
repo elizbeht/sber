8305429f4a938604e16aea2b316870e8a172941a
elizbeht <161321437+elizbeht@users.noreply.github.com>
2024-02-26 18:52:06 +0600

    create app

---
+++ lib/cards.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+import 'app_theme/app_theme.dart';
+
+class Cards extends StatelessWidget {
+  const Cards({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 166,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          _buildCard('assets/images/sberprime.png', AppTheme.sberPrimeCardTitle,
+              AppTheme.sberPrimeCardPayment, AppTheme.sberPrimeCardAmount,
+              isFirstCard: true),
+          _buildCard('assets/images/percent.png', AppTheme.percentCardTitle,
+              AppTheme.percentCardPayment, AppTheme.percentCardAmount),
+          _buildCard('assets/images/percent.png', AppTheme.paymentsCard,
+              AppTheme.paymentPeriod, AppTheme.paymentsValue),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCard(
+      String imagePath, String title, String payment, String amount,
+      {bool isFirstCard = false}) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: isFirstCard ? 0 : 8),
+      child: SizedBox(
+        width: 216,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 22, bottom: 22),
+          child: DecoratedBox(
+            decoration: ShapeDecoration(
+              color: AppTheme.whiteColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              shadows: const [
+                BoxShadow(
+                  color: AppTheme.shadowColor1,
+                  blurRadius: 14,
+                  offset: Offset(0, 8),
+                  spreadRadius: 0,
+                ),
+                BoxShadow(
+                  color: AppTheme.shadowColor2,
+                  blurRadius: 10,
+                  offset: Offset(0, 2),
+                  spreadRadius: 0,
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 14, vertical: 16),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        imagePath,
+                        width: 36,
+                        height: 36,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(width: 12),
+                      Text(
+                        title,
+                        style: AppTheme.tabBarTextStyle,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 12),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(
+                    payment,
+                    style: AppTheme.cardTextStyle,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 2),
+                  child: Text(
+                    amount,
+                    style: AppTheme.smallTextStyle,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/categories_chips.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'App_Theme/app_theme.dart';
+
+class CategoriesChips extends StatefulWidget {
+  @override
+  _CategoriesChipsState createState() => _CategoriesChipsState();
+}
+
+class _CategoriesChipsState extends State<CategoriesChips> {
+  Set<String> selectedCategories = {};
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 8.0,
+      runSpacing: 2.0,
+      children: AppTheme.categories.map((category) {
+        return ChoiceChip(
+          label: Text(category),
+          selected: selectedCategories.contains(category),
+          onSelected: (isSelected) {
+            setState(() {
+              if (isSelected) {
+                selectedCategories.add(category);
+              } else {
+                selectedCategories.remove(category);
+              }
+            });
+          },
+          backgroundColor: AppTheme.chipColor,
+          selectedColor: Colors.grey[500],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+            side: BorderSide.none,
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
